bdf76108de12e8bcf634dca702f3603aa15b665e
huilan1210 <85428703+huilan1210@users.noreply.github.com>
2022-03-10 16:53:22 +0800

    Update read_EC_matrix.m

--- FNvalueCalculation/read_EC_matrix.m
+++ FNvalueCalculation/read_EC_matrix.m
@@ -1,7 +1,7 @@
 %plot_corrected_couplings('C:\Users\Administrator\Desktop\matalb-data\EC\1nh8.eij')
-params = read_params('3lcb.eij');
+params = read_params('5mo4.eij');
 figure
 plot_coupling_scores(params);
-saveas(gcf,'3lcb_eij.png');
+saveas(gcf,'5mo4_eij.png');
 
-export_couplings_json(read_params('3lcb.eij'), '3lcb_eij.json')
+export_couplings_json(read_params('5mo4.eij'), '5mo4_eij.json')
